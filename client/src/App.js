cdfe544c696fb9bcb0a0873d211a7e5f81d72cd8
wwbarton <wbartonw5@gmail.com>
2022-02-03 11:09:33 -0700

    init git

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand"> </div>
+        <div className="current-operand"></div>
+      </div>
+      <button className="span-two"></button>
+      <button>DEL </button>
+    </div>
+  );
+}
+
+export default App;
