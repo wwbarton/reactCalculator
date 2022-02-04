252a171e9251716359ba9a9d7db4e50b0942c38a
wwbarton <wbartonw5@gmail.com>
2022-02-03 21:27:25 -0700

    styling and states

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,54 @@
+import "./styles.css";
+import { useReducer } from "react";
+const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE: "delete-digit",
+  EVALUATE: "evaluate",
+};
+
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        currentOperand: `${currentOperand || ""}  ${payload.digit}`,
+      };
+  }
+}
+
 function App() {
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
+    reducer,
+    {}
+  );
+  dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1 } });
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"> </div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">
+          {previousOperand} {operation}{" "}
+        </div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
-      <button className="span-two"></button>
+      <button className="span-two">AC</button>
       <button>DEL </button>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
     </div>
   );
 }
